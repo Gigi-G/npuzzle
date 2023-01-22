1c402bc80c73bfebdad7ee67786cb6ba114ad9bc
Gigi-G <seminara.luigi@gmail.com>
2023-01-22 21:12:08 +0100

    fix: IDA_STAR

--- utils/IDA_STAR.py
+++ utils/IDA_STAR.py
@@ -3,13 +3,16 @@
 # date: 2023-01-18
 # tags: python, IDA*, search, algorithm
 
+explored = set()
+
 def IDA_STAR(initial_state,heuristic):
     """IDA* search"""
     
     threshold = heuristic(initial_state)
     
     while True:
-        result = search(initial_state, heuristic, threshold)
+        explored.clear()
+        result = search(initial_state, heuristic, initial_state.cost, threshold)
         if result[0] == "cutoff":
             threshold = result[1]
         elif result == "failure":
@@ -18,17 +21,14 @@ def IDA_STAR(initial_state,heuristic):
             return (result[1], result[2], result[3])
     
 
-def search(initial_state, heuristic, threshold, nodes_expanded = 0, max_search_depth = 0):
-    state = initial_state
+def search(state, heuristic, g, threshold, nodes_expanded = 0, max_search_depth = 0):
     print(f"****** State with threshold = {threshold}******")
     state.display()
     
     if state.is_goal():
         return ("found", state, nodes_expanded, max_search_depth)
-    
-    nodes_expanded += 1
-    
-    estimated_cost = state.cost + heuristic(state)
+        
+    estimated_cost = g + heuristic(state)
     
     if estimated_cost > threshold:
         print(f"Estimated cost = {estimated_cost} > threshold = {threshold}")
@@ -36,15 +36,17 @@ def search(initial_state, heuristic, threshold, nodes_expanded = 0, max_search_d
         return ("cutoff", estimated_cost)
     
     min = float("inf")
-    for neigbhor in state.expand(RLDU= False):
-        if neigbhor.cost > max_search_depth:
-            max_search_depth = neigbhor.cost
-        result = search(neigbhor, heuristic, threshold, nodes_expanded, max_search_depth)
-        if result[0] == "found":
-            return result
-        elif result[0] == "cutoff":
-            if result[1] < min:
-                min = result[1]
+    for neigbhor in state.expand(RLDU = False):
+        if neigbhor not in explored:
+            explored.add(neigbhor)
+            if neigbhor.cost > max_search_depth:
+                max_search_depth = neigbhor.cost
+            result = search(neigbhor, heuristic, neigbhor.cost, threshold, nodes_expanded+1, max_search_depth)
+            if result[0] == "found":
+                return result
+            elif result[0] == "cutoff":
+                if result[1] < min:
+                    min = result[1]
         
     if min == float("inf"):
         return "failure"
